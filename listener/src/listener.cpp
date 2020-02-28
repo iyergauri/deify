01df549b55c919bdfc0e064eddf9659e6224a4d3
Gauri Iyer <iyergauri@gmail.com>
2020-02-27 16:56:31 -0800

    in-file documentation added, incl. instructions

--- listener/src/listener.cpp
+++ listener/src/listener.cpp
@@ -1,3 +1,33 @@
+/************************
+ * Filename: listener.cpp
+ * 
+ * Students:
+ * - Gauri Iyer
+ * - Sam Scudder
+ * - Shreeman Hariharan
+ * 
+ * HW 11: Implementation and Robot-Focused Testing
+ * 
+ * Description: This file defines the behavior for the listener node in
+ * the listener ROS package. This node navigates a Turtlebot that is 
+ * trying to escape from humanity by asking for help. The Turtlebot only
+ * has one automated task: move forward. Humans are required to remove
+ * obstacles from its path and recommend which way to turn.
+ * 
+ * Instructions:
+ * (Don't forget to source devel/setup.bash in every terminal!)
+ * In 5 terminals, run:
+ *    1. roscore
+ *    2. roslaunch turtlebot_bringup minimal.launch
+ *    3. roslaunch astra_launch astra_pro.launch
+ *    4. roslaunch sound_play soundplay_node.py
+ *    5. roslaunch pocketsphinx turtlebot_voice_cmd.launch
+ * In a 6th terminal, inside the workspace containining listener, run:
+ *    - catkin_make
+ *    - rosrun listener listener
+ ************************/
+
+
 #include "ros/ros.h"
 #include <std_msgs/String.h>
 #include <ros/console.h>
@@ -9,27 +39,15 @@
 
 using namespace std;
 
+// Node publisher + PCL and Speech subscribers
 ros::Publisher cmdpub;
 ros::Subscriber depthSubscriber;
 ros::Subscriber speechRecognitionSubscriber;
-//sound_play::SoundClient sc;
-
-bool done = false;
-double turnDir = 1;
 
 // Depth data from PCL
 float currDepth = 999;
 float depthX = 0;
 float depthY = 0;
-
-// Blob data for potential target blob from CMVision
-double maxBlob = 0;
-double center = 0.0;
-double leftBlob = -1;
-double rightBlob = -1;
-double top = -1;
-double bottom = -1;
-double blobCount = 0;
 
 // Thresholds for depth calculation
 const float MIN_Y = 0.1;
@@ -38,14 +56,36 @@ const float MIN_X = -0.2;
 const float MAX_X = 0.2;
 const float TOO_CLOSE = 0.8;
 
+// Keep track of blocked state so robot can ask for help
 bool blocked = false;
 
-void sleepok(int t, ros::NodeHandle &nh)
- {
+/************************
+ * void sleepok(int t, ros::NodeHandle &nh)
+ * 
+ * Purpose: sleep while the Turtlebot "says" things, so that it doesn't
+ * accidentally interpret its' own speech as commands.
+ * 
+ * @param int t - number of seconds to sleep for
+ * @param ros::NodeHandle &nh - ensures the handle is still active
+ * 
+ * @return none
+ ************************/
+void sleepok(int t, ros::NodeHandle &nh){
    if (nh.ok())
        sleep(t);
- }
+}
 
+/************************
+ * Name: void depthCallback(const sensor_msgs::ImageConstPtr& depthMsg)
+ *
+ * Purpose: This function detects the closest object to the TurtleBot based 
+ * on the Astra camera's point cloud.
+ *
+ * @param const sensor_msgs::ImageConstPtr& depthMsg - contains the depth 
+ * information from the Point Cloud Library node.
+ *
+ * @return none
+************************/
 void depthCallback(const sensor_msgs::ImageConstPtr& depthMsg) {
   // ROS_INFO_STREAM("depth callback");
   sound_play::SoundClient sc;
@@ -92,36 +132,56 @@ void depthCallback(const sensor_msgs::ImageConstPtr& depthMsg) {
   }
   //ROS_INFO_STREAM(currDepth);
   
-  ROS_INFO_STREAM(currDepth);
+
+  // Actions change depending on current/past state of robot.
+  // Case 1: Robot is newly obstructed. Announce that robot is stuck, and 
+  // ask for help.
   geometry_msgs::Twist T;
   if (currDepth <= TOO_CLOSE and !blocked) {
-    // ROS_INFO_STREAM("stop");
     blocked = true;
     T.linear.x = 0;
     cmdpub.publish(T);
     sc.say("Help, I'm stuck! Could you remove the obstacle, or tell me to go left or right?");
     sleepok(7, n);
   }
+
+  // Case 2: Robot is previously obstructed, and has already announced that it
+  // needed help. Do nothing; wait for human to help!
   else if (currDepth <= TOO_CLOSE and blocked) {
     // do nothing
   } 
+
+  // Case 3: Robot is not obstructed.
   else {
+
+    // Case 3A: Robot was previously obstructed - thank the human!
     if (blocked) {
       sleepok(2, n);
       sc.say("Thank you for unblocking me!");
       sleepok(2, n);
     }
+
+    // Case 3B: Robot was not previously obstructed. Just move forward.
     blocked = false;
-    // ROS_INFO_STREAM("move");
     T.linear.x = 0.3;
     T.angular.z = 0;
     cmdpub.publish(T);
-    sleepok(1, n);
+    // sleepok(1, n);
   }
 }
 
+/************************
+ * Name: void voiceCallback(const std_msgs::String recogMsg)
+ *
+ * Purpose: This function detects human voice commands, limited to
+ * "left" and "right".
+ *
+ * @param const std_msgs::String recogMsg - contains the audio  
+ * information as text from the pocketsphinx node.
+ *
+ * @return none
+************************/
 void voiceCallback(const std_msgs::String recogMsg) {
-   
   geometry_msgs::Twist T;
   ros::NodeHandle n;
   sound_play::SoundClient sc;
@@ -131,18 +191,18 @@ void voiceCallback(const std_msgs::String recogMsg) {
     T.angular.z = 2;
     // T.linear.x = 0.5;
     cmdpub.publish(T);
-    sleepok(1, n);
+    // sleepok(1, n);
     // sc.say("Turning");
-    sleepok(2, n);
+    // sleepok(2, n);
     
   }
   else if (recogMsg.data == "right") {
     ROS_INFO_STREAM("FOUND RIGHT");
     T.angular.z = -2;
     cmdpub.publish(T);
-    sleepok(1, n);
+    // sleepok(1, n);
     // sc.say("Turning");
-    sleepok(2, n);
+    // sleepok(2, n);
   }
   else {
     sleepok(1, n);
@@ -151,21 +211,17 @@ void voiceCallback(const std_msgs::String recogMsg) {
   }
 }
 
-void maneuver() {
-  geometry_msgs::Twist T;
-  if (currDepth <= TOO_CLOSE) {
-    ROS_INFO_STREAM("too close");
-    T.linear.x = 0;
-    cmdpub.publish(T);
-  }
-  else {
-    //ROS_INFO_STREAM("move");
-    T.linear.x = 0.5;
-    T.angular.z = 0;
-    cmdpub.publish(T);
-  }
-}
-
+/************************
+ * Name: int main(int argc, char** argv)
+ *
+ * Purpose: This function initiates the Turtlebot's escape by 
+ * receiving camera and voice metrics.
+ *
+ * @param int argc - unused
+ * @param char** argv - unused
+ *
+ * @return 0 (exit on success)
+************************/
 int main(int argc, char** argv) {
   ros::init(argc, argv, "hello_ros");
   ros::NodeHandle n;
@@ -184,7 +240,7 @@ int main(int argc, char** argv) {
     //sleepok(1, n);
     //sc.say("Hello");
     //sleepok(2, n);
-    //rate.sleep();
+    rate.sleep();
   }
   
   return 0;
